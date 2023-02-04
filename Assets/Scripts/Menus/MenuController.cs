6a17ced32012c3688fa5d70857e32e0969f795ec
Sylfr Tan <JOHN0044@e.ntu.edu.sg>
2023-02-05 03:27:42 +0800

    Collapsed UI into one scene. Added interactions.

---
+++ Assets/Scripts/Menus/MenuController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject MainMenuGroup;
+    [SerializeField] private GameObject SettingsGroup;
+    [SerializeField] private GameObject ArenaSelectGroup;
+    [SerializeField] private GameObject CreditsGroup;
+    private GameObject CurrGroup;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayButtonClicked()
+    {
+        MainMenuGroup.SetActive(false);
+        CurrGroup = ArenaSelectGroup;
+        ArenaSelectGroup.SetActive(true);
+    }
+
+    public void SettingsButtonClicked()
+    {
+        MainMenuGroup.SetActive(false);
+        CurrGroup = SettingsGroup;
+        SettingsGroup.SetActive(true);
+    }
+
+    public void CreditsButtonClicked()
+    {
+        MainMenuGroup.SetActive(false);
+        CurrGroup = CreditsGroup;
+        CreditsGroup.SetActive(true);
+    }
+
+    public void BackButtonClicked()
+    {
+        CurrGroup.SetActive(false);
+        CurrGroup = MainMenuGroup;
+        MainMenuGroup.SetActive(true);
+    }
+}
